21c8d68bf7ec1e44b0b5b43e7e9875a4a088d9ba
ZHANSAYA BORTANOVA <123213674+CreatoriOS@users.noreply.github.com>
2023-02-04 04:29:29 +0400

    Add files via upload

---
+++ CryptotradingAppSwiftUI/CryptotradingAppSwiftUI/CryptotradingAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptotradingAppSwiftUIApp.swift
+//  CryptotradingAppSwiftUI
+//
+//  Created by Zhansaya Bortanova on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptotradingAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptotradingAppSwiftUI/CryptotradingAppSwiftUI/Extensions/Color.swift
@@ -0,0 +1,27 @@
+//
+//  Color.swift
+//  CryptotradingAppSwiftUI
+//
+//  Created by Zhansaya Bortanova on 04/02/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    
+    static let theme  = ColorTheme()
+    
+    
+}
+struct ColorTheme{
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText  = Color("SecondaryTextColor")
+    
+    
+}
+
